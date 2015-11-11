4cb9cee6c65e85df3d09b1235bb180317b07e442
Alexander Ostroumov <alexanderostroumov@yandex.ru>
2015-11-11 12:44:46 +0300

    Let's go

---
+++ MathProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class MathMethods
+    {
+        public static double RightTriangleSquare(double firstSide, double secondSide, double thirdSide)
+        {
+            double square;
+            return square;
+        }
+    }
+}
