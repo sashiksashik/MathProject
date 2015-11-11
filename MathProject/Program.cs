da87a8c914994165eadde4c39670469357a54048
Alexander Ostroumov <alexanderostroumov@yandex.ru>
2015-11-11 23:26:14 +0300

    Исправлен метод проверки сузествовать п/у треугольника

--- MathProject/Program.cs
+++ MathProject/Program.cs
@@ -16,19 +16,18 @@ namespace MathProject
 
     public class MathMethods
     {
+        private static double DOUBLE_COMPUTATION_ERROR = 0.0000005;
         public static double RightTriangleSquare(double firstSide, double secondSide, double thirdSide)
         {
             if (firstSide <= 0) throw new ArgumentException("Side one should be greater than zero.");
             if (secondSide <= 0) throw new ArgumentException("Side one should be greater than zero.");
             if (thirdSide <= 0) throw new ArgumentException("Side one should be greater than zero.");
-            if (!(Math.Pow(firstSide, 2) == Math.Pow(secondSide, 2) + Math.Pow(thirdSide, 2) ||
-                Math.Pow(secondSide, 2) == Math.Pow(firstSide, 2) + Math.Pow(thirdSide, 2) ||
-                Math.Pow(thirdSide, 2) == Math.Pow(secondSide, 2) + Math.Pow(firstSide, 2)))
+            double max = Math.Max(firstSide, Math.Max(secondSide, thirdSide));
+            if ((Math.Pow(firstSide, 2) + Math.Pow(secondSide, 2) + Math.Pow(thirdSide, 2) - 2 * Math.Pow(max, 2)) > DOUBLE_COMPUTATION_ERROR)
             {
                 throw new ArgumentException("This is not a right-angled triangle.");
             }
-            double max = Math.Max(firstSide, Math.Max(secondSide, thirdSide));
-            return (firstSide*secondSide*thirdSide) / (2 * max);
+            return (firstSide * secondSide * thirdSide) / (2 * max);
         }
     }
 }
