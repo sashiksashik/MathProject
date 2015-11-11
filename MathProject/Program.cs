1ec9047e632548238f5ec01b8ed722a69abb4020
Alexander Ostroumov <alexanderostroumov@yandex.ru>
2015-11-11 22:54:11 +0300

    Убрал лишнюю проверку, изменил способ вычисления площади.

--- MathProject/Program.cs
+++ MathProject/Program.cs
@@ -21,27 +21,14 @@ namespace MathProject
             if (firstSide <= 0) throw new ArgumentException("Side one should be greater than zero.");
             if (secondSide <= 0) throw new ArgumentException("Side one should be greater than zero.");
             if (thirdSide <= 0) throw new ArgumentException("Side one should be greater than zero.");
+            if (!(Math.Pow(firstSide, 2) == Math.Pow(secondSide, 2) + Math.Pow(thirdSide, 2) ||
+                Math.Pow(secondSide, 2) == Math.Pow(firstSide, 2) + Math.Pow(thirdSide, 2) ||
+                Math.Pow(thirdSide, 2) == Math.Pow(secondSide, 2) + Math.Pow(firstSide, 2)))
+            {
+                throw new ArgumentException("This is not a right-angled triangle.");
+            }
             double max = Math.Max(firstSide, Math.Max(secondSide, thirdSide));
-            if (2 * max > (firstSide + secondSide + thirdSide)) throw new ArgumentException("This triangle does not exists.");
-            if (firstSide == max)
-            {
-                if (Math.Pow(firstSide, 2) == Math.Pow(secondSide, 2) + Math.Pow(thirdSide, 2)){
-                    return (secondSide * thirdSide) / 2;
-                }
-            }
-            if (secondSide == max)
-            {
-                if (Math.Pow(secondSide, 2) == Math.Pow(firstSide, 2) + Math.Pow(thirdSide, 2)){
-                    return (firstSide * thirdSide) / 2;
-                }
-            }
-            if (thirdSide == max)
-            {
-                if (Math.Pow(thirdSide, 2) == Math.Pow(firstSide, 2) + Math.Pow(secondSide, 2)){
-                    return (firstSide * secondSide) / 2;
-                }
-            }
-            throw new ArgumentException("This is not a right-angled triangle.");
+            return (firstSide*secondSide*thirdSide) / (2 * max);
         }
     }
 }
